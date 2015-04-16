cb66e0435aff786d52dd73f2f501e68b297601cc
Shengzhe Chen <schen@ancestry.com>
2015-04-16 16:04:04 -0700

    add project

---
+++ Bluetooth/Bluetooth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bluetooth
+//
+//  Created by Shengzhe Chen on 4/16/15.
+//  Copyright (c) 2015 Shengzhe Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Bluetooth/Bluetooth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bluetooth
+//
+//  Created by Shengzhe Chen on 4/16/15.
+//  Copyright (c) 2015 Shengzhe Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
